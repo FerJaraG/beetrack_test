7582e4c0267f7a5cc66628bebd8386dd5609c066
FerJaraG <mfjaragalleguillos@gmail.com>
2018-06-15 17:20:46 -0400

    add comments on methods

--- app/controllers/api/v1/gps/vehicles_controller.rb
+++ app/controllers/api/v1/gps/vehicles_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Gps::VehiclesController < ApplicationController
     def show
+        #aqui se construyen los marcadores del mapa con la información de la BD
         @waypoints = Waypoint.joins(:vehicle).where("waypoints.created_at = (select MAX(w1.created_at) from waypoints w1 where w1.vehicle_id = vehicles.id)").select("waypoints.vehicle_id, waypoints.id, waypoints.created_at, waypoints.latitude, waypoints.longitude")
 
         @hash = Gmaps4rails.build_markers(@waypoints) do |waypoint, marker|
